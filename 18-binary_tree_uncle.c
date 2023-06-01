d8de83a06d13f32a86e5e96dc29f04b810d6a8da
Huming-bird <sonugaahmed@gmail.com>
2023-06-01 09:17:15 -0700

    made changes to tasks 18 and 21

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,15 +10,28 @@
  * Return: Number
  */
 
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
 	binary_tree_t *grandparent = node->parent->parent;
 
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (grandparent->left == NULL || grandparent->right == NULL)
+	{
 		return (NULL);
+	}
+
 	if (grandparent->left == node->parent)
+	{
 		return (grandparent->right);
-	if (grandparent->right == node->parent)
+	}
+	else if (grandparent->right == node->parent)
+	{
 		return (grandparent->left);
+	}
 	return (NULL);
 }
