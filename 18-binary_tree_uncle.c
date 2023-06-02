102ca77e2e056e9cc782d24b556eb48f4eaeced3
solaure2tt <117128033+solaure2tt@users.noreply.github.com>
2023-06-01 21:58:29 -0400

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,12 +13,13 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent = node->parent->parent;
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *grandparent = node->parent->parent;
+	
 
 	if (grandparent->left == NULL || grandparent->right == NULL)
 	{
